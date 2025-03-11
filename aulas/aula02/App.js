8a8e2279ab12d035afb189bd8e8d60e6a79cc524
unknown <2314290130@ACADEMICO.IESB.BR>
2025-03-10 22:03:10 -0300

    codigos da aula02

---
+++ aulas/aula02/App.js
@@ -0,0 +1,12 @@
+import Flexbox from "./screens/Flexbox";
+import GaleriaScreen from "./screens/GaleriaScreen";
+import PadraoAndroid from "./screens/PadraoAndroid";
+import PadraoIOS from "./screens/PadraoIOS";
+
+function App() {
+  return (
+    <GaleriaScreen />
+  );
+}
+
+export default App;
